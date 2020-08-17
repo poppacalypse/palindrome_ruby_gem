64975f88337ff85cfe4d7ceeb30e9c1c3e8fad9d
Carl Poppa <poppacalypse@gmail.com>
2020-08-17 15:28:35 +0800

    Initialise repository

---
+++ lib/poppacalypse_palindrome.rb
@@ -0,0 +1,6 @@
+require "poppacalypse_palindrome/version"
+
+module PoppacalypsePalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/poppacalypse_palindrome/version.rb
@@ -0,0 +1,3 @@
+module PoppacalypsePalindrome
+  VERSION = "0.1.0"
+end
